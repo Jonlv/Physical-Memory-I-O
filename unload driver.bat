c1ce6feda36a55ada204af6f732a2d0b9fe2d728
debugger <yuuaresokrul@gmail.com>
2017-12-01 22:24:16 -0600

    Update unload driver.bat

--- unload driver.bat
+++ unload driver.bat
@@ -1,4 +1,4 @@
 "driver loader" snake system32\drivers\snake.sys -unload_uninstall
 timeout 1
 del "%WINDIR%\system32\drivers\snake.sys"
-copy "%WINDIR%\system32\drivers\3ware.sys" "%WINDIR%\system32\drivers\snake.sys"
+copy "%WINDIR%\system32\drivers\uefi.sys" "%WINDIR%\system32\drivers\snake.sys"
